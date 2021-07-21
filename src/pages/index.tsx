6e3bdc1a9efe34bbe6da49ed4c07604d66da9213
williamspacefire <william@meycup.com>
2021-07-21 16:23:22 +0000

    Changed from function component to class component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,25 +5,31 @@ import Footer from '../views/footer/footer'
 import { GetStaticProps } from 'next'
 import { Post } from '../entities/Post'
 import { postsImpl } from '../main/dependencies'
+import React from 'react'
 
 export const getStaticProps: GetStaticProps = async context => {
     return { props: { posts: postsImpl.getPosts() } }
 }
 
-export default function Index(props: { posts: Post[] }) {
-    const { posts } = props
-
-    return (
-        <>
-            <Header
-                title={
-                    config.site.name +
-                    ' - Um Compilado de conhecimentos em programação'
-                }
-                homePage={true}
-            />
-            <PostPage posts={posts} />
-            <Footer />
-        </>
-    )
+type IndexProps = {
+    posts: Post[]
 }
+
+export default class IndexPage extends React.Component<IndexProps> {
+    render() {
+        return (
+            <>
+                <Header
+                    title={
+                        config.site.name +
+                        ' - Um Compilado de conhecimentos em programação'
+                    }
+                    homePage={true}
+                />
+                <PostPage posts={this.props.posts} />
+                <Footer />
+            </>
+        )
+    }
+}
+
