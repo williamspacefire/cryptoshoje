a4a65f918d346f7796d675aa248df91ae540cf8e
William Spacefire <william@meycup.com>
2021-07-25 17:46:21 -0300

    Moved the content of post page to PostPageContent

--- src/views/post/index.tsx
+++ src/views/post/index.tsx
@@ -1,7 +1,26 @@
+import { Box, Flex, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { Post } from '../../entities/Post'
+import MarkdownRender from './MarkdownRender'
+import { PostThumbnail } from './PostThumbnail'
+import PostTitle from './PostTitle'
 
-export default class PostPage extends React.Component {
+export default class PostPageContent extends React.Component<Post> {
     render() {
-        return <></>
+        return (
+            <>
+                <Flex justify='center' align='center' p='10'>
+                    <Box w='3xl'>
+                        <VStack spacing='20px'>
+                            <PostTitle title={this.props.title} />
+                            <PostThumbnail thumbnail={this.props.thumbnail} />
+                            <Box>
+                                <MarkdownRender {...this.props} />
+                            </Box>
+                        </VStack>
+                    </Box>
+                </Flex>
+            </>
+        )
     }
 }
