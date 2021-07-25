6dd986e4a21c7e73e0cdb43d3f86a3f86d5d1262
William Spacefire <william@meycup.com>
2021-07-25 17:46:49 -0300

    Implemented PostPageContent component

--- src/pages/[post].tsx
+++ src/pages/[post].tsx
@@ -1,53 +1,31 @@
 import React from 'react'
 import Header from '../views/header'
-import { Flex, Heading, VStack, Box } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/react'
-import ReactMarkdown from 'react-markdown'
 import { GetStaticProps } from 'next'
-import gfm from 'remark-gfm'
 import { Post } from '../entities/Post'
 import Footer from '../views/footer'
-import { postsImageDirectory, postsImpl } from '../main/dependencies'
-import { PostsComponents } from '../use_cases/posts_components'
-import PostTitle from '../views/post/PostTitle'
-import { PostThumbnail } from '../views/post/PostThumbnail'
-const highlight = require('rehype-highlight')
+import { postsImpl } from '../main/dependencies'
+import PostPageContent from '../views/post'
+
+export default class PostPage extends React.Component<Post> {
+    render() {
+        return (
+            <>
+                <Header title={this.props.title} />
+                <PostPageContent {...this.props} />
+                <Footer />
+            </>
+        )
+    }
+}
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const postId = params?.post as string
     postsImpl.setPostId(postId)
     const post = postsImpl.getPost()
 
-    if (post) return { props: { post } }
+    if (post) return { props: post }
 
     return { notFound: true }
-}
-
-const Posts = (props: { post: Post }) => {
-    const { post } = props
-
-    return (
-        <>
-            <Header title={post.title} />
-            <Flex justify='center' align='center' p='10'>
-                <Box w='3xl'>
-                    <VStack spacing='20px'>
-                        <PostTitle title={post.title} />
-                        <PostThumbnail thumbnail={post.thumbnail} />
-                        <Box>
-                            <ReactMarkdown
-                                remarkPlugins={[gfm]}
-                                rehypePlugins={[highlight]}
-                                components={PostsComponents.components()}>
-                                {post?.content}
-                            </ReactMarkdown>
-                        </Box>
-                    </VStack>
-                </Box>
-            </Flex>
-            <Footer />
-        </>
-    )
 }
 
 export async function getStaticPaths() {
@@ -56,5 +34,3 @@ export async function getStaticPaths() {
         paths: postsImpl.getHomePagePaths(),
     }
 }
-
-export default Posts
