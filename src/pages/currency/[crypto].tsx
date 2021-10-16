873310b5941bc1a81f2229f671f3c412291df66f
William Spacefire <william@meycup.com>
2021-10-15 21:54:59 -0300

    Returned to Static Props, using SWR to get crypto price realtime

--- src/pages/currency/[crypto].tsx
+++ src/pages/currency/[crypto].tsx
@@ -2,25 +2,44 @@ import React from 'react'
 import Header from '../../views/header'
 import { CryptoInformation } from '../../entities/cryptointerface'
 import NomicsApiImpl from '../../adapters/NomicsApi'
-import { GetServerSideProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import ConvertComponent from '../../views/crypto/ConvertComponent'
 import Head from 'next/head'
 import { formatter } from '../../use_cases/currency'
+import useSWR, { SWRConfig } from 'swr'
 
-export const getServerSideProps: GetServerSideProps = async context => {
+export const getStaticProps: GetStaticProps = async context => {
     const api = new NomicsApiImpl()
-    const cryptoName = context!.params!.crypto as string
-    console.log('CryptoName: ' + cryptoName)
-    const cryptoResponse = await api.getSingleCrypto(cryptoName.toUpperCase())
-    console.log(cryptoResponse)
-    return { props: { cryptos: JSON.stringify(cryptoResponse) } }
+    const cryptoName = (await context!.params!.crypto) as string
+    const cryptoResponse = await api.getSingleCrypto(cryptoName)
+
+    return {
+        props: {
+            fallback: {
+                '/api/currency/': cryptoResponse,
+            },
+            cryptoName: cryptoName,
+        },
+        revalidate: 1000,
+    }
 }
 
-export default function IndexPage(props: { cryptos: string }) {
-    var data: CryptoInformation | null = null
-    if (props.cryptos != undefined) {
-        data = JSON.parse(props.cryptos) as CryptoInformation
+function CryptoComponent(props: {
+    cryptoName: string
+    fallback: CryptoInformation
+}) {
+    const heathers = new Headers()
+    heathers.set('X-Currency', props.cryptoName)
+
+    const fetcherOptions = {
+        method: 'GET',
+        headers: heathers,
     }
+    const fetcher = (args: RequestInfo) =>
+        fetch(args, fetcherOptions).then(res => res.json())
+    const { data, error } = useSWR('/api/currency/', fetcher, {
+        refreshInterval: 3000,
+    })
 
     return (
         <>
@@ -40,9 +59,23 @@ export default function IndexPage(props: { cryptos: string }) {
     )
 }
 
-//export const getStaticPaths: GetStaticPaths = async () => {
-//    return {
-//        paths: [],
-//        fallback: true,
-//    }
-//}
+export default function IndexPage(props: {
+    fallback: CryptoInformation
+    cryptoName: string
+}) {
+    return (
+        <SWRConfig value={{ fallback: props.fallback }}>
+            <CryptoComponent
+                cryptoName={props.cryptoName}
+                fallback={props.fallback}
+            />
+        </SWRConfig>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking',
+    }
+}
